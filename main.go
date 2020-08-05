63b9199101689210202405659bc831f0dc57eabf
Forrest <Forrestmillerj@gmail.com>
2020-08-05 16:02:56 -0500

    Imported functions from drive and docs package.

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/fomiller/Go-Coverletter-CLI/docs"
+
 	"github.com/fomiller/Go-Coverletter-CLI/drive"
 )
 
 func main() {
-	drive.CreateTemplateCopy()
+	templateCopyId := drive.CreateTemplateCopy()
+	fmt.Println("This is the template Copy id: ", templateCopyId)
+	templateId := docs.UpdateTemplateFile(templateCopyId)
+
+	fmt.Println("Successfully updated document: ", templateId)
 }
