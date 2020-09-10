66e4e6403f2c681b942bbbcfcaf73d44429474ee
Forrest <Forrestmillerj@gmail.com>
2020-09-10 16:23:21 -0500

    delete now uses global flag -n for specifiying file name

--- cmd/delete.go
+++ cmd/delete.go
@@ -19,19 +19,26 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/fomiller/scribe/drive"
 	"github.com/spf13/cobra"
 )
 
-var deleteFile string
+// var deleteFile string
 
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "Delete File from Google Drive",
 	Run: func(cmd *cobra.Command, args []string) {
+		if NewFileName == "" {
+			prompt := &survey.Input{
+				Message: "What is the name of the file you want to delete?",
+			}
+			survey.AskOne(prompt, &NewFileName)
+		}
 		// get docId for file to be deleted
-		docId, err := drive.GetFileId(deleteFile)
+		docId, err := drive.GetFileId(NewFileName)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -42,7 +49,7 @@ var deleteCmd = &cobra.Command{
 			log.Fatal(err)
 		} else {
 			// if err is nil
-			fmt.Println("File Deleted")
+			fmt.Printf("%v Deleted", NewFileName)
 		}
 	},
 }
@@ -59,5 +66,5 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	deleteCmd.Flags().StringVarP(&deleteFile, "delete", "p", "", "Insert your file name here.")
+	// deleteCmd.Flags().StringVarP(&deleteFile, "delete", "p", "", "Insert your file name here.")
 }
