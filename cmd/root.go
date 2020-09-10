f1032449c4d1b391f476629e925a80ec32d10d80
Forrest <Forrestmillerj@gmail.com>
2020-09-10 12:59:56 -0500

    Added a list of commands to choose from when no commands/arguments are declared'

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
@@ -23,9 +24,24 @@ var rootCmd = &cobra.Command{
 	Long:  `Scribe is a CLI for interfacing with templates using the Google Docs and Google Drive API.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) {
-	// 	fmt.Println(DownloadFile)
-	// },
+	Run: func(cmd *cobra.Command, args []string) {
+		var command string
+		prompt := &survey.Select{
+			Message: "Choose a command to run:",
+			Options: []string{"create", "parse", "delete"},
+		}
+		survey.AskOne(prompt, &command)
+
+		// based on survey selection run the corresponding command
+		switch command {
+		case "create":
+			createCmd.Run(cmd, args)
+		case "parse":
+			parseCmd.Run(cmd, args)
+		case "delete":
+			deleteCmd.Run(cmd, args)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,7 +63,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.coverletter.yaml)")
 	rootCmd.PersistentFlags().BoolVarP(&DlFile, "download", "d", false, "download file")
 	rootCmd.PersistentFlags().StringVarP(&TemplateName, "template", "t", "", "Enter the name of the template you would like to use.")
-	rootCmd.PersistentFlags().StringVarP(&NewFileName, "name", "n", "nil", "Enter the name of the new file.")
+	rootCmd.PersistentFlags().StringVarP(&NewFileName, "name", "n", "", "Enter the name of the new file.")
 	// rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "author name for copyright attribution")
 	// rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
 	// rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
