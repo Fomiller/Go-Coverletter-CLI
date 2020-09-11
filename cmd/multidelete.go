480720e073ad742a918b910b2b836d587bf5e5d4
Forrest <Forrestmillerj@gmail.com>
2020-09-11 13:29:30 -0500

    added saftey features for not allowing to delete all files in google drive

--- cmd/multidelete.go
+++ cmd/multidelete.go
@@ -25,23 +25,42 @@ to quickly create a Cobra application.`,
 		prompt := &survey.MultiSelect{
 			Message:  "What files do you want to delete:",
 			Options:  fileNameList,
-			PageSize: 15,
+			PageSize: 10,
 		}
-		survey.AskOne(prompt, &filesToDelete)
 
-		// delete files that were selected to be deleted
-		for _, v := range filesToDelete {
-			// get docId for file to be deleted
-			docId, err := drive.GetFileId(v)
-			if err != nil {
-				log.Fatal(err)
+		survey.AskOne(prompt, &filesToDelete, survey.WithKeepFilter(true))
+
+		// If the user selects ALL Files to be deleted double check if they would like to delete ALL files
+		if len(fileNameList) == len(filesToDelete) {
+			// deletePrompt := &survey.Confirm{
+			// 	Message: fmt.Sprintf("You have selected ALL of the files in your drive, %v files. Are You sure you want to delete all of these files? You CAN NOT undo these changes", len(filesToDelete)),
+			// }
+			// deleteAll := false
+			// survey.AskOne(deletePrompt, deleteAll)
+			// if deleteAll == true {
+			// 	fmt.Println("Deleting all files")
+			// }
+			fmt.Println(`
+	Sorry! As a saftey precaution Scribe is not allowed to delete all of your files.
+	
+	If you wish to delete all of your files please do so using the Google Drive/Docs application.
+	
+	`)
+		} else {
+			// delete files that were selected to be deleted
+			for _, v := range filesToDelete {
+				// get docId for file to be deleted
+				docId, err := drive.GetFileId(v)
+				if err != nil {
+					log.Fatal(err)
+				}
+				// delete file
+				err = drive.DeleteFile(docId)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("%v Deleted\n", v)
 			}
-			// delete file
-			err = drive.DeleteFile(docId)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf("%v Deleted\n", v)
 		}
 	},
 }
