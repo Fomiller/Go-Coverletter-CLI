6cedea2ee18145d20343bc3f82c0a2148a300d22
Forrest <Forrestmillerj@gmail.com>
2020-09-15 16:40:34 -0500

    command to list and select from all files

--- cmd/list.go
+++ cmd/list.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/fomiller/scribe/drive"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +14,16 @@ var listCmd = &cobra.Command{
 	Short: "list out files in your drive.",
 	Long:  `List will return you a list of the files located in your google drive without a search criteria.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		fileNameList := drive.ListFileNames()
+		file := ""
+		prompt := &survey.Select{
+			Message:  "Select a file:",
+			Options:  fileNameList,
+			PageSize: 10,
+		}
+
+		survey.AskOne(prompt, &file, survey.WithKeepFilter(true))
+		fmt.Printf("You chose: %v", file)
 	},
 }
 
