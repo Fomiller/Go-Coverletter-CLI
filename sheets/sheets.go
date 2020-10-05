c92182133415d60ea5edbbc59f9dc924f90977e5
Forrest <Forrestmillerj@gmail.com>
2020-10-05 13:52:22 -0500

    modified getClient function to retrieve token.json from inside './sheets/'

--- sheets/sheets.go
+++ sheets/sheets.go
@@ -1,4 +1,4 @@
-package main
+package sheets
 
 import (
 	"encoding/json"
@@ -8,18 +8,50 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/fomiller/scribe/config"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/sheets/v4"
 )
 
+var sheetsSrv *sheets.Service
+
+func init() {
+	b, err := ioutil.ReadFile(fmt.Sprintf("./sheets/%v", config.Scribe.Credentials.Sheets))
+	if err != nil {
+		log.Fatalf(`%v
+ 
+	Please navigate to https://console.cloud.google.com/apis/credentials, to create and download credentials for a 0Auth client ID.
+
+	Save your new credentials as 'credentials.json' inside the scribe/sheets folder.
+			
+	Once credentials are saved to the correct location run 'go install' from the applications root directory.
+			
+	to test your applicaton run 'scribe' in your terminal
+
+`, err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, sheets.SpreadsheetsScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	sheetsSrv, err = sheets.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+}
+
 // Retrieve a token, saves the token, then returns the generated client.
 func getClient(config *oauth2.Config) *http.Client {
 	// The file token.json stores the user's access and refresh tokens, and is
 	// created automatically when the authorization flow completes for the first
 	// time.
-	tokFile := "token.json"
+	tokFile := "./sheets/token.json"
 	tok, err := tokenFromFile(tokFile)
 	if err != nil {
 		tok = getTokenFromWeb(config)
@@ -69,29 +101,29 @@ func saveToken(path string, token *oauth2.Token) {
 	json.NewEncoder(f).Encode(token)
 }
 
-func main() {
-	b, err := ioutil.ReadFile("credentials.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
+func RunSheets() {
+	// b, err := ioutil.ReadFile("credentials.json")
+	// if err != nil {
+	// 	log.Fatalf("Unable to read client secret file: %v", err)
+	// }
 
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, sheets.SpreadsheetsScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-	client := getClient(config)
+	// // If modifying these scopes, delete your previously saved token.json.
+	// config, err := google.ConfigFromJSON(b, sheets.SpreadsheetsScope)
+	// if err != nil {
+	// 	log.Fatalf("Unable to parse client secret file to config: %v", err)
+	// }
+	// client := getClient(config)
 
-	srv, err := sheets.New(client)
-	if err != nil {
-		log.Fatalf("Unable to retrieve Sheets client: %v", err)
-	}
+	// srv, err := sheets.New(client)
+	// if err != nil {
+	// 	log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	// }
 
 	// Prints the names and majors of students in a sample spreadsheet:
 	// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
 	spreadsheetId := "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
 	readRange := "Class Data!A2:E"
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	resp, err := sheetsSrv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve data from sheet: %v", err)
 	}
