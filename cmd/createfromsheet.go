ebcb169a03bcbcd5058aa7855070059688a69432
Forrest <Forrestmillerj@gmail.com>
2020-10-08 17:07:25 -0500

    added new command

--- cmd/createfromsheet.go
+++ cmd/createfromsheet.go
@@ -1,3 +1,18 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package cmd
 
 import (
@@ -8,9 +23,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// createfromfileCmd represents the createfromfile command
-var createfromfileCmd = &cobra.Command{
-	Use:   "createfromfile",
+// createfromsheetCmd represents the createfromsheet command
+var createfromsheetCmd = &cobra.Command{
+	Use:   "createfromsheet",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -36,11 +51,11 @@ to quickly create a Cobra application.`,
 			}
 
 			// create file for each row in spreadsheetData
-			docs.CreateFile(fmt.Sprintf("test-%v", FieldMap["Student Name"]), "FROM FILE TEMPLATE", FieldMap, false)
+			docs.CreateFile(fmt.Sprintf("test-%v", FieldMap["Student Name"]), TemplateName, FieldMap, false)
 		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(createfromfileCmd)
+	rootCmd.AddCommand(createfromsheetCmd)
 }
