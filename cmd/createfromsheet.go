1b0ea84c2551837a14ad97f16a47a01a8c3f7813
Forrest <Forrestmillerj@gmail.com>
2020-10-09 11:48:36 -0500

    change how the filename is generated

--- cmd/createfromsheet.go
+++ cmd/createfromsheet.go
@@ -49,9 +49,10 @@ to quickly create a Cobra application.`,
 				fvalue := fmt.Sprint(vv.FieldValue)
 				FieldMap[fkey] = fvalue
 			}
-
+			// create file name by adding the TemplateName and the Unique File ID value together
+			Name = fmt.Sprintf("%v - %v", TemplateName, FieldMap["Unique File ID"])
 			// create file for each row in spreadsheetData
-			docs.CreateFile(fmt.Sprintf("test-%v", FieldMap["Student Name"]), TemplateName, FieldMap, false)
+			docs.CreateFile(Name, TemplateName, FieldMap, DlFile)
 		}
 	},
 }
