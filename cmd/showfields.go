12ee0c0d6aebce338d4899c2917c7874c29d5203
Forrest <Forrestmillerj@gmail.com>
2020-10-16 14:22:15 -0500

    updated description

--- cmd/showfields.go
+++ cmd/showfields.go
@@ -12,13 +12,7 @@ import (
 // showfieldsCmd represents the showfields command
 var showfieldsCmd = &cobra.Command{
 	Use:   "showfields",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Return a list of all fields inside a template.",
 	Run: func(cmd *cobra.Command, args []string) {
 		// if template flag is not used prompt for the template name
 		if TemplateName == "" {
